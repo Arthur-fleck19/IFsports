87199af1bdbc877fdc0eb42f539bd5c92465cbc9
Josué Frauzino Vital de Souza <20251CTI-NH0024@nh.ifsul.edu.br>
2026-09-08 16:48:56 -0300

    Código banco de dados loja terminado, organizado e definitivo(2)

--- codigo_banco.sql
+++ codigo_banco.sql
@@ -10,6 +10,14 @@ CREATE TABLE Produtos(
     preco DECIMAL(10, 2) NOT NULL
 );
 
+CREATE TABLE Usuarios(
+	id_usuarios INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    nome VARCHAR(75) NOT NULL,
+    tipo VARCHAR(6) NOT NULL,
+    senha VARCHAR(300) NOT NULL
+);
+
 CREATE TABLE Pedidos(
 	id_pedidos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_usuarios INT NOT NULL,
@@ -30,14 +38,6 @@ CREATE TABLE Item_pedidos(
     FOREIGN KEY (id_pedidos) REFERENCES Pedidos(id_pedidos)
 );
 
-CREATE TABLE Usuarios(
-	id_usuarios INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    email VARCHAR(320) NOT NULL,
-    nome VARCHAR(75) NOT NULL,
-    tipo VARCHAR (6) NOT NULL,
-    senha VARCHAR(300) NOT NULL
-);
-
 CREATE TABLE Carrinho(
 	id_carrinho INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_usuarios INT NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE Carrinho(
 CREATE TABLE item_carrinho(
 	id_item_carrinho INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_carrinho INT NOT NULL,
-    id_produto INT NOT NULL,
+    id_produtos INT NOT NULL,
     quantidade INT NOT NULL,
     FOREIGN KEY(id_carrinho) REFERENCES Carrinho(id_carrinho),
     FOREIGN KEY(id_produtos) REFERENCES Produtos(id_produtos)
