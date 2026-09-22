4ceeb44f87e3ab90ad822fe828f5ebc7d91b744d
Rebeca Duarte Campos <20251CTI-NH0015@nh.ifsul.edu.br>
2026-09-22 17:00:50 -0300

    Banco de dados atualizado

--- codigo_banco.sql
+++ codigo_banco.sql
@@ -1,34 +1,40 @@
+
 CREATE DATABASE IFsports;
 USE IFsports;
 
+
 CREATE TABLE Produtos(
-	id_produtos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(150) NOT NULL, 
+    id_produtos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(150) NOT NULL,
     descricao VARCHAR(500) NOT NULL,
     imagem VARCHAR(1000) NOT NULL,
     estoque INT NOT NULL,
-    preco DECIMAL(10, 2) NOT NULL
+    preco DECIMAL(10, 2) NOT NULL,
+    tipo VARCHAR(15) NOT NULL
 );
 
+
 CREATE TABLE Usuarios(
-	id_usuarios INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_usuarios INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(320) NOT NULL,
     nome VARCHAR(75) NOT NULL,
     tipo VARCHAR(6) NOT NULL,
     senha VARCHAR(300) NOT NULL
 );
 
+
 CREATE TABLE Pedidos(
-	id_pedidos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_pedidos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_usuarios INT NOT NULL,
     statusPed VARCHAR(10) NOT NULL,
     valor DECIMAL(10, 2) NOT NULL,
-    data_pedido DATE NOT NULL, 
+    data_pedido DATE NOT NULL,
     FOREIGN KEY (id_usuarios) REFERENCES Usuarios(id_usuarios)
 );
 
+
 CREATE TABLE Item_pedidos(
-	id_item_pedidos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_item_pedidos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_produtos INT NOT NULL,
     id_pedidos INT NOT NULL,
     quantidade INT NOT NULL,
@@ -38,15 +44,17 @@ CREATE TABLE Item_pedidos(
     FOREIGN KEY (id_pedidos) REFERENCES Pedidos(id_pedidos)
 );
 
+
 CREATE TABLE Carrinho(
-	id_carrinho INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_carrinho INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_usuarios INT NOT NULL,
     data_criacao DATE NOT NULL,
     FOREIGN KEY(id_usuarios) REFERENCES Usuarios(id_usuarios)
 );
 
+
 CREATE TABLE item_carrinho(
-	id_item_carrinho INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_item_carrinho INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     id_carrinho INT NOT NULL,
     id_produtos INT NOT NULL,
     quantidade INT NOT NULL,
